4cc1c0a7ffbc44f03b1cc79100a379daab5dd4fb
KnewHow <948170910@qq.com>
2019-01-25 21:25:25 +0800

    complete ScaleData and Persion

---
+++ chapter07-class/ScaleData.h
@@ -0,0 +1,49 @@
+#ifndef SCALEDATA_H
+#define SCALEDATA_H
+#include<string>
+#include<iostream>
+
+struct ScaleData{
+  std :: string bookNo;
+
+  unsigned unitSold = 0;
+
+  double revenue = 0.0;
+
+  std :: string isbn() const {
+    return this->bookNo;
+  };
+
+  ScaleData& combine(const ScaleData& rhs) {
+    unitSold += rhs.unitSold;
+    revenue += rhs.revenue;
+    return *this;
+  }
+
+  double avgPrice() const {
+    if(unitSold != 0) {
+      return revenue / unitSold;
+    } else {
+      return 0.0;
+    }
+  }
+};
+
+ScaleData add(const ScaleData& s1, const ScaleData& s2) {
+  ScaleData sum = s1;
+  sum.combine(s1);
+  return sum;
+}
+
+std :: ostream &print(std :: ostream& os, const ScaleData& item) {
+  os << item.isbn() << " " << item.unitSold << " " << item.revenue << " " << item.avgPrice();
+  return os;
+}
+
+std :: istream &read(std :: istream& is, ScaleData& item) {
+  double price = 0.0;
+  is >> item.bookNo >> item.unitSold >> price;
+  item.revenue = item.unitSold * price;
+  return is;
+}
+#endif
---
+++ chapter07-class/ScaleDataDemo.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<string>
+#include "ScaleData.h"
+
+using std :: cout;
+using std :: string;
+using std :: cin;
+using std :: endl;
+
+int main () {
+  return 0;
+}
